63fa1e28e127c7191b89952bbdaafd218abafb5c
Marco Monteiro <mpmont@gmail.com>
2012-07-15 14:26:53 +0100

    added autoload

---
+++ config/autoload.php
@@ -0,0 +1,10 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ *
+ * @original_author     Craig Campbell
+ * @author              Marco Monteiro <marco@marcomonteiro.net>
+ * @license		        http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+$autoload['helper'] = array('ChromePhp');
